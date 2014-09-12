9f08a09763378c54b6e29a800184ad51435472ea
Zack Shoylev <zack.shoylev@rackspace.com>
2014-09-12 14:58:27 -0500

    Fixes

--- neutron-sdk-example/src/main/java/org/jclouds/neutron/example/domain/Network.java
+++ neutron-sdk-example/src/main/java/org/jclouds/neutron/example/domain/Network.java
@@ -16,8 +16,6 @@
  */
 package org.jclouds.neutron.example.domain;
 
-import javax.inject.Named;
-
 import com.google.common.collect.ImmutableSet;
 
 /**
@@ -30,17 +28,16 @@ import com.google.common.collect.ImmutableSet;
 public class Network {
 
    private String id;
-   private NetworkStatus status;
+   // Enumerable
+   private String status;
    private ImmutableSet<String> subnets;
 
    private String name;
-   @Named("admin_state_up")
    private Boolean adminStateUp;
    private Boolean shared;
-   @Named("tenant_id")
    private String tenantId;
 
-   @Named("provider:network_type")
-   private NetworkType networkType;
+   // Enumerable
+   private String networkType;
 
 }
